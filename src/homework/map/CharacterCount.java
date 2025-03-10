24378c15bbfa67822588b51661e94ce6a81071b6
charLin <118043579+Code-Min159@users.noreply.github.com>
2025-03-10 12:09:43 +0800

    Update CharacterCount.java
    update hw for map

--- src/homework/map/CharacterCount.java
+++ src/homework/map/CharacterCount.java
@@ -8,12 +8,7 @@ public class CharacterCount {
         // 填寫代碼：統計每個字符出現的次數
         for (char c : input.toCharArray()) {
             // 填寫代碼：將字符作為key，出現次數作為value
-            
-            if(map.isempty){// 第一次出現
-                map.put(c,count);
-            }else{
-                map.put(c,(count + 1));
-        }
+            map.put(c, map.getOrDefault(c, defaultValue.0)+1);
         
         // 輸出每個字母出現的次數 預期結果 {
         //  h=1,
@@ -24,6 +19,6 @@ public class CharacterCount {
         //  r=1,
         //  d=1
         //}
-        System.out.println("");
+        System.out.println(map);
     }
 }
